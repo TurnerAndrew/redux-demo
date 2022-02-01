1a2c044ed6fd7681665441cb10cf6d40366717fa
Andrew Turner <turner.andrew0@gmail.com>
2022-02-01 11:24:28 -0600

    mapStateToProps on Name, added name to details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
-const Details = () => {
-  return <div></div>;
+const Details = (props) => {
+
+    const name = useSelector(store => store.name)
+
+  return (
+    <div>
+        <h1>{props.name}</h1>
+    </div>
+    )
 };
 
 export default Details;
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { connect } from 'react-redux'
+import { addAge } from '../redux/reducer';
 
-const Main = () => {
+const Name = (props) => { 
 
-  
+  const [name, setName] = useState('')
+
+  const addName = () => {
+    props.addName(name)
+  }
 
   return (
     <div>
-        <h1>Enter your name:<input type='text'></input></h1>
-        <button>Save</button>
+        <h1>Enter your name:<input type='text' onChange={e => {setName(e.target.value)}}></input></h1>
+        <button onClick={addName}>Save</button>
     </div>
-)
+  )
 };
 
-export default Main;
+const mapStateToProps = (state) => {
+  return {
+    age: state.age,
+  }
+}
+
+export default connect(mapStateToProps, {addAge})(Name)
