2580c403e005988b37f82a7a50a83c4b3f17aa5b
Andrew Turner <turner.andrew0@gmail.com>
2022-02-01 11:14:26 -0600

    reducer setup

---
+++ src/redux/reducer.js
@@ -0,0 +1,48 @@
+const initialState = {
+    name: null,
+    age: null,
+    location: null
+}
+
+const ADD_NAME = 'ADD_NAME'
+const ADD_AGE = 'ADD_AGE'
+const ADD_LOCATION = 'ADD_LOCATION'
+
+export const addName = (name) => {
+    return {
+        type: ADD_NAME,
+        payload: name
+    }
+}
+
+export const addAge = (age) => {
+    return {
+        type: ADD_AGE,
+        payload: age
+    }
+}
+
+export const addLocation = (location) => {
+    return {
+        type: ADD_LOCATION,
+        payload: location
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_NAME:
+            const name = action.payload
+            return{...state, name: name}
+        case ADD_AGE:
+            const age = action.payload
+            return {...state, age: age}
+        case ADD_LOCATION:
+            const location = action.payload
+            return {...state, location: location}            
+        default:
+            return state
+    }
+}
+
+export default reducer
