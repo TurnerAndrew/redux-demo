8335d1f6abbe2e1afe22fb599b535c2562e75fcf
Andrew Turner <turner.andrew0@gmail.com>
2022-02-01 11:28:47 -0600

    wrapped app in provder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Name from './components/Name'
+import Age from './components/Age'
+import Location from './components/Location'
+import Details from './components/Details'
 
 function App() {
   return (
     <div>
-        
+      <Name/>
+      <Age/>
+      <Location/>
+      <Details/>        
     </div>
   );
 }
