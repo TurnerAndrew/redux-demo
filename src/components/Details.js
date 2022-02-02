09dbf49a9830c707d2483e3e4ae76fa29957ade4
Andrew Turner <turner.andrew0@gmail.com>
2022-02-02 09:45:14 -0600

    redux complete

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,11 +5,13 @@ const Details = (props) => {
 
     const name = useSelector(store => store.name)
     const age = useSelector(store => store.age)
+    const location = useSelector(store => store.location)
 
   return (
     <div>
         <h1>Name: <h2>{name}</h2></h1>
         <h1>Age: <h2>{age}</h2></h1>
+        <h1>Location: <h2>{location}</h2></h1>
     </div>
     )
 };
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux'
+import { addLocation } from '../redux/reducer'
 
-const Location = () => {
+const Location = (props) => {
+
+  const [location, setLocation] = useState()
+
+  const addLocation = () => {
+    props.addLocation(location)
+  }
+
   return (
     <div>
-        <h1>Enter your location:<input type='text'></input></h1>
-        <button>Save</button>
+        <h1>Enter your location:<input type='text' onChange={(e => setLocation(e.target.value))}></input></h1>
+        <button onClick={addLocation}>Save</button>
     </div>
     )
 };
 
-export default Location;
+const mapStateToProps = (state) => {
+  return {
+    location: state.location
+  }
+}
+
+export default connect( mapStateToProps, {addLocation})(Location)
