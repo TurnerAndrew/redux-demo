4445763482bc72c699dee59906dc2a7c602ea097
Andrew Turner <turner.andrew0@gmail.com>
2022-02-03 10:34:32 -0600

    final styling

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Details = () => {
   return (
     <div className={'form-div'}>
-        <h1 className={'label'}>Name: </h1><h2 className={'detail-info'}></h2>
-        <h1 className={'label'}>Age: </h1><h2 className={'detail-info'}></h2>
-        <h1 className={'label'}>Location:</h1> <h2 className={'detail-info'}></h2>
+        <div className={'form-div details'}>
+        <div className={'form-container'}><h1 className={'label'}>Name: </h1><h2 className={'detail-info'}></h2></div>
+        <div className={'form-container'}><h1 className={'label'}>Age: </h1><h2 className={'detail-info'}></h2></div>
+        <div className={'form-container'}><h1 className={'label'}>Location: </h1> <h2 className={'detail-info'}></h2></div>
+    </div>
     </div>
     )
 };
